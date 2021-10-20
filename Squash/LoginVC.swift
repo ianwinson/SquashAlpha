a021bd0cc8f7b02c60de7ffe778ff735eff62f8b
Ian Winson <iankwinson@gmail.com>
2021-10-20 12:59:21 -0500

    Initial Commit of Combined code

---
+++ Squash/LoginVC.swift
@@ -0,0 +1,11 @@
+// Project: Squash
+// Course: CS371L
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Squash/LogoAnimationView.swift
+++ Squash/LogoAnimationView.swift
@@ -1,30 +1,30 @@
-// Project: Squash
-// Course: CS371L
+//
+//  LogoAnimationView.swift
+//  Squash
+//
+//  Created by Jordan Ferguson on 10/14/21.
+//
 
 import UIKit
 
-// Initial View Controller: VC that animates logo on LaunchScreen
-class ViewController: UIViewController {
+class LogoAnimationView: UIViewController {
     
-    // Create an image view to make this VC identical to LaunchScreen
     private let imageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 414, height: 414))
         imageView.image = UIImage(named: "SquashIconStatic")
         return imageView
     } ()
 
-    // After loading the view add the logo
+    // Do any additional setup after loading the view.
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
     }
     
-    // Executed after viewDidLoad to animate icon
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.center = view.center
         
-        // Wait half second then animate
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
             self.animate()
         })
@@ -32,23 +32,24 @@ class ViewController: UIViewController {
     
     // Animate logo to get larger and disappear
     private func animate() {
-        
-        // Calculate dimensions of larger logo
         UIView.animate(withDuration: 1, animations: {
             let size = self.view.frame.size.width * 2
             let diffX = size - self.view.frame.size.width
             let diffY = self.view.frame.size.height - size
+            
             self.imageView.frame = CGRect(x: -(diffX/2), y: diffY/2, width: size, height: size)
         })
         
-        // Make logo fade into LoginVC
         UIView.animate(withDuration: 1.5, animations: {
             self.imageView.alpha = 0
-        }, completion: { done in
+        }, completion: {
+            done in
             if done {
-                // Segue from this VC to LoginVC
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: {
-                    self.performSegue(withIdentifier: "LogoSegueIdentifier", sender: nil)
+                    let viewController = ViewController()
+                    viewController.modalTransitionStyle = .crossDissolve
+                    viewController.modalPresentationStyle = .fullScreen
+                    self.present(viewController, animated: true)
                 })
             }
         })
